18ed91edf76e79a8c392a4da5fe0fb41720505a0
iamshaunjp <shaun@thenetninja.co.uk>
2024-10-09 16:15:04 +0100

    lesson-3

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -6,10 +6,20 @@
 </head>
 <body>
   <h2>Currently Available Ninjas</h2>
-  
+  <p>{{ $greeting }}</p>
+
   <ul>
-    <li>Ninjas here</li>
+    <li>
+      <a href="/ninjas/{{ $ninjas[0]["id"] }}">
+        {{ $ninjas[0]["name"] }}
+      </a>
+    </li>
+    <li>
+      <a href="/ninjas/{{ $ninjas[1]["id"] }}">
+        {{ $ninjas[1]["name"] }}
+      </a>
+    </li>
   </ul>
-  
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,17 @@ Route::get('/', function () {
 });
 
 Route::get('/ninjas', function () {
-  return view('ninjas.index');
+  $ninjas = [
+    ["name" => "mario","skill" => 75, "id" => "1"],
+    ["name" => "luigi","skill" => 45, "id" => "2"],
+  ];
+
+  return view('ninjas.index', ["greeting" => "hello", "ninjas" => $ninjas]);
+});
+
+Route::get('/ninjas/{id}', function ($id) {
+  // do something with the id
+  // e.g. fetch a record from the db
+
+  return view('ninjas.show', ['id' => $id]);
 });
