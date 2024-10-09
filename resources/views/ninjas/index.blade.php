bad24d0c5c99847a380fbd20a73cbc522bee4786
iamshaunjp <shaun@thenetninja.co.uk>
2024-10-09 14:11:39 +0100

    lesson-2

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ninja Network</title>
+</head>
+<body>
+  <h2>Currently Available Ninjas</h2>
+  
+  <ul>
+    <li>Ninjas here</li>
+  </ul>
+  
+</body>
+</html>
