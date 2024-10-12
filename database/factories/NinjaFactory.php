4cb3ef73a3a969e1f3169dada1ab68abda5329b4
iamshaunjp <shaun@thenetninja.co.uk>
2024-10-12 15:49:23 +0100

    lesson-10

--- database/factories/NinjaFactory.php
+++ database/factories/NinjaFactory.php
@@ -17,7 +17,9 @@ class NinjaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'skill' => fake()->numberBetween(1, 100),
+            'bio' =>fake()->realText(500),
         ];
     }
 }
