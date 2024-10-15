e1771fa28f4aa7a85bdc9d612dd10d37eb15078f
iamshaunjp <shaun@thenetninja.co.uk>
2024-10-15 13:20:53 +0100

    lesson-20

--- resources/views/ninjas/create.blade.php
+++ resources/views/ninjas/create.blade.php
@@ -10,7 +10,8 @@
     <input 
       type="text" 
       id="name" 
-      name="name" 
+      name="name"
+      value="{{ old('name') }}" 
       required
     >
 
@@ -19,7 +20,8 @@
     <input 
       type="number" 
       id="skill" 
-      name="skill" 
+      name="skill"
+      value="{{ old('skill') }}"
       required
     >
 
@@ -28,16 +30,16 @@
     <textarea
       rows="5"
       id="bio" 
-      name="bio" 
+      name="bio"
       required
-    ></textarea>
+    >{{ old('bio') }}</textarea>
 
     <!-- select a dojo -->
     <label for="dojo_id">Dojo:</label>
     <select id="dojo_id" name="dojo_id" required>
       <option value="" disabled selected>Select a dojo</option>
       @foreach ($dojos as $dojo)
-        <option value="{{ $dojo->id }}">
+        <option value="{{ $dojo->id }}" {{ $dojo->id == old('dojo_id') ? 'selected' : '' }}>
           {{ $dojo->name }}
         </option>
       @endforeach
@@ -46,6 +48,13 @@
     <button type="submit" class="btn mt-4">Create Ninja</button>
 
     <!-- validation errors -->
+    @if ($errors->any())
+      <ul class="px-4 py-2 bg-red-100">
+        @foreach ($errors->all() as $error)
+          <li class="my-2 text-red-500">{{ $error }}</li>
+        @endforeach
+      </ul>
+    @endif
     
   </form>
 </x-layout>
